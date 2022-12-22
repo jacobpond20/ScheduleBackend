d8b7f27b71f694db85bb22d8afe35ca58e39bd4e
Jacob Pond <60118043+jpond@users.noreply.github.com>
2022-12-22 12:58:42 -0600

    12/22

--- connect.js
+++ connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const db = require('mongodb').Db;
 
 //const uri = "mongodb://localhost:27017/CompanyTracker?retryWrites=true&w=majority"; //test for capstone
-const uri = "mongodb+srv://user1:user1@scheduleapp.dw63x.mongodb.net/ScheduleApp?retryWrites=true&w=majority";
+const uri = "mongodb+srv://admin:admin@scheduleapp.atk7n.mongodb.net/test";
 
 const connectDB = async()=> {
     await mongoose.connect(uri);
